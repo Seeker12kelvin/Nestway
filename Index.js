5551eee78ac70466377095b9e76cfae95fb0915a
C-G <kelvindonye@gmail.com>
2025-07-05 12:58:43 +0100

    Yeah

---
+++ Index.js
@@ -0,0 +1,6 @@
+const openEL = document.getElementById("open-menu")
+const closeEl = document.getElementById("close-menu")
+
+openEL.addEventListener("clicked", () => {
+  openEL.style.display = "none"
+})
